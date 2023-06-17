734ec72bf4cb515bb29746a931dd0296de338e22
amitmonver <amitmonver95@gmail.com>
2023-06-17 17:24:58 +0500

    practice.dart

---
+++ practice.dart
@@ -0,0 +1,385 @@
+
+import 'dart:io';
+void main()
+{
+//question 1
+//  print("Enter the length:");
+//   int length = int.parse(stdin.readLineSync()!);
+
+//   print("Enter the breadth:");
+//   int breadth = int.parse(stdin.readLineSync()!);
+
+// if(length==breadth)
+// {
+// print("Its a Sqaure Value");
+
+// }
+
+// else 
+// {
+
+//   print("Its a rectangle value");
+// }
+
+
+
+
+
+// question 2
+// int age1=4;
+// int age2=5;
+
+// if(age1>age2)
+// {
+//   print("Age1 is the oldest");
+//   print("Age 2 is the youngest");
+// }
+
+// else
+// {
+// print("Age2 is the oldest");
+// print("Age 1 is the youngest");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 3
+
+// int nO_OfclasseS=16;
+// int  nO_OfclasseSaTTended=10;
+// num percentage= (10/16)*100;
+
+// if(percentage<75)
+// {
+//   print("You are not allowe to sit in exam");
+// }
+
+// else
+// {
+//   print("you are allowed to sit in exam");
+// }
+
+// print("Your Attendance in percenatge is :");
+// print(percentage);
+
+
+
+
+
+
+
+
+//question 4
+
+
+// int year=2001;
+
+// if(year%400==0)
+// {
+// print("its leaping year");
+// }
+
+// else
+// {
+//   print("its not a leaping year");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// question 5
+
+// num temp = 11;
+// if(temp<0)
+// {
+// print("freeezing weather");
+// }
+
+// else if(temp>0 && temp<=10)
+// {
+// print("very cold weather");
+
+// }
+
+// else if(temp>=10 && temp<=20)
+// {
+// print("then cold weather");
+
+// }
+
+// else if(temp>=20 && temp<=30)
+// {
+// print("then normal in temperature");
+
+// }
+
+// else if(temp>=30 && temp<=40)
+// {
+// print("its hot");
+
+// }
+
+// else if(temp>40 )
+// {
+// print("very hot");
+
+// }
+
+
+
+
+
+// question 6
+
+// print("Enter a character: ");
+ 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 7
+
+// num customer_id=1001;
+// String Name="Amit";
+// num unit=800;
+
+// if(unit>=199)
+// {
+//   num netbill= 1.20*unit;
+//   print("$customer_id");
+//   print(Name);
+//   print(unit);
+//   print("Amount_charges: @1.20 ");
+//   print("net bill amount is: $netbill");
+// }
+
+// else if(unit>=200 && unit<=400)
+// {
+//   num netbill= 1.50*unit;
+// print(customer_id);
+//   print(Name);
+//   print(unit);
+//   print("Amount_charges: @1.50 ");
+// print("net bill amount is: $netbill");
+// }
+
+// else if(unit>=400 && unit<=600)
+// {
+//   num netbill= 1.80*unit;
+// print(customer_id);
+//   print(Name);
+//   print(unit);
+//   print("Amount_charges: @1.80 ");
+// print("net bill amount is: $netbill");
+
+// }
+// else if(unit>600)
+// {
+//     num netbill= 2.00*unit;
+// print(customer_id);
+//   print(Name);
+//   print(unit);
+//   print("Amount_charges: @2.00 ");
+// print("net bill amount is: $netbill");
+// }
+
+
+
+
+
+
+//question # 8
+
+// num maths=45;
+// print(" marks of maths : $maths");
+
+// num english=71.2;
+// print(" marks of english : $english");
+
+// num science=65.3;
+// print(" marks of science:  $science" );
+
+// num phy=63;
+// print(" marks of physics:  $phy");
+
+// num chem=56;
+// print("marks of chemistry: $chem ");
+
+
+// num percent = ((maths+english+science+phy+chem)/500)*100;
+
+
+
+// String name="Amit";
+// print(" your name: $name");
+// int rollnum=1001;
+// print("your roll no: $rollnum ");
+// print("Your percentage :  $percent");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 9
+
+// int number =13;
+// if(number%2==0)
+// {
+
+//   print("number is even");
+//   print("now checking if its divisible by 5 or not");
+
+// if(number%5==0)
+// {
+// print("number divisible by 5");
+
+// }
+
+// else if(number%5==1)
+// {
+
+// print("number is not divisible by 5");
+
+// }
+
+
+
+// if(number%2==1)
+// {
+
+//   print("number is even");
+//   print("now checking if its divisible by 7 or not");
+
+// if(number%7==0)
+// {
+// print("number divisible by 7");
+
+// }
+
+// else
+// {
+
+// print("number is not divisible by 7");
+
+// }
+
+
+
+
+// question number 10
+
+// int num1= 10;
+// int num2= 12;
+//  int num3=4;
+
+//  if(num1>num2&& num1> num3)
+//  {
+// print ("number 1 is greatest : $num1");
+
+//  }
+
+//  if(num2>num1&& num2> num3)
+//  {
+// print ("number 2 is greatest : $num2");
+
+//  }
+
+
+//  if(num3>num1&& num3> num2)
+//  {
+// print ("number 3 is greatest : $num3");
+
+//  }
+
+
+//  if(num1<num2&& num1< num3)
+//  {
+// print ("number 1 is lowest : $num1");
+
+//  }
+
+
+// if(num2<num1&& num2< num3)
+//  {
+// print ("number 2 is lowest : $num2");
+
+//  }
+
+// if(num3<num1&& num3< num2)
+//  {
+// print ("number 3 is lowest : $num3");
+
+//  }
+
+
+
+
+// question 12
+
+
+num temp=15;
+print("temperature in : celcuis : $temp");
+
+print("converting in fahrenheight");
+
+temp=(temp*9)/5+32;
+num temp2= temp;
+
+print("temperature in fahrenheight: $temp2");
+}
+
+
+
+
+
+
+
+
+
+
+
